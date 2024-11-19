94c880af63764fc11b162150ffdde22973c5b7bd
Ayman Mohamed <darknaya.am@gmail.com>
2024-11-19 16:32:20 +0200

    edit on gemini api key

--- lib/core/networking/api_constance.dart
+++ lib/core/networking/api_constance.dart
@@ -3,6 +3,6 @@ class ApiConstance {
   static const String contentType = 'application/json';
   static const String geminiBaseUrl =
       "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:";
-  static const String geminiApiKey = "AIzaSyD5DfnuQ6buRh-o4cq0PXEAVcUA6wVMs2g";
+  static const String geminiApiKey = "AIzaSyBMtP8VLBhbKMMrsso8s8y1uxymEuHy0Js";
   static const String googleBaseUrl = "https://www.google.com/";
 }
